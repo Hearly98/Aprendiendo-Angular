0e60a643be80ac1a9dcc1855f835b4157cfcb79a
Sebastian <shuertas981@gmail.com>
2023-11-21 07:39:13 -0500

    Aprendiendo la estructura del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,6 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent { 
   title = 'miprimerproyecto';
 }
