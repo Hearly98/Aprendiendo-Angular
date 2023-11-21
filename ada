681fbdbab10891dfa8647fa06b3f600054ca2dfb
Shp981 <shuertas981@gmail.com>
2023-11-21 18:05:53 -0500

    Importando y exportando modulos

---
+++ ada
@@ -0,0 +1,17 @@
+[33mcommit 6f8d2ef71bc25d0c81eebda1202c6583c30dc5b3[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m
+Author: Shp981 <shuertas981@gmail.com>
+Date:   Tue Nov 21 13:59:25 2023 -0500
+
+    Creando modulo header
+
+[33mcommit 0e60a643be80ac1a9dcc1855f835b4157cfcb79a[m[33m ([m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m
+Author: Sebastian <shuertas981@gmail.com>
+Date:   Tue Nov 21 07:39:13 2023 -0500
+
+    Aprendiendo la estructura del proyecto
+
+[33mcommit 2b14bae4658047ce486092d8aafbf875145672ee[m
+Author: Sebastian <shuertas981@gmail.com>
+Date:   Tue Nov 21 00:26:49 2023 -0500
+
+    initial commit
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
-@Component({
+@Component({//decorador->extender la funcionalidad de algo
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  currentDate: Date | null = null;
+  constructor() { }
 
+resultado: number =0;
+
+  ngOnInit(): void {
+      this.currentDate =new Date();
+
+  }
+  sumar(a: number, b:number){
+    this.resultado= a+b ;
+
+  }
 }
